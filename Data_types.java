3d7412847b47a29a937a05d969a30053624741b5
Aditi Vyas <aditivyas8602@gmail.com>
2026-09-04 12:32:51 +0530

    Added program of data types and literals

---
+++ Data_types.java
@@ -0,0 +1,43 @@
+public class Data_types {
+    public static void main(String[] args) {
+        // Integer data type
+        int myNum = 5;
+        System.out.println("Integer: " + myNum);
+
+        // Float data type
+        float myFloat = 5.99f;
+        System.out.println("Float: " + myFloat);
+
+        // Double data type
+        double myDouble = 5.99;
+        System.out.println("Double: " + myDouble);
+
+        // Character data type
+        char myChar = 'A';
+        System.out.println("Character: " + myChar);
+
+        // Boolean data type
+        boolean myBool = true;
+        System.out.println("Boolean: " + myBool);
+
+        // String data type
+        String myString = "Hello, World!";
+        System.out.println("String: " + myString);
+
+        // Long data type
+        long myLong = 15000000000L;
+        System.out.println("Long: " + myLong);
+
+        // Short data type
+        short myShort = 5000;
+        System.out.println("Short: " + myShort);
+
+        // Byte data type
+        byte myByte = 100;
+        System.out.println("Byte: " + myByte);
+
+        // binary literal
+        int a=0b101; 
+        System.out.println("Binary: " + a);
+    } 
+}
