b7e93b6ff8405daab13f2b52a6e073ecae0dc4c4
Aditi Vyas <aditivyas8602@gmail.com>
2026-09-04 17:51:39 +0530

    Added the program of do while loop

---
+++ Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("i is: " + i);
+            i++;
+        } while (i < 5);
+    }
+    
+}
